242410f265aeab634481bf09f97c446b0fdeabb0
DoctorJambo <arhipov09090az@gmail.com>
2026-01-27 19:13:42 +0200

    implemented MainApp class

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
-public class MainApp {
+import core.basesyntax.machines.Bulldozer;
+import core.basesyntax.machines.Excavator;
+import core.basesyntax.machines.Truck;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = {
+                new Bulldozer(),
+                new Excavator(),
+                new Truck()
+        };
+
+        for (int i = 0; i < machines.length; i++) {
+            machines[i].doWork();
+            machines[i].stopWork();
+        }
+    }
 }
